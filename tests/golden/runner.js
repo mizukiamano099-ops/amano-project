db2f0fa083c01ff4bf14154f3ced140a7ebf446d
mizukiamano099-ops <mizukiamano099@gmail.com>
2025-11-20 19:20:59 +0900

    Update runner.js

--- tests/golden/runner.js
+++ tests/golden/runner.js
@@ -8,7 +8,7 @@
 import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
-import { compileIR } from "../../src/cli/index.js";
+import { compileIR } from "../../src/compiler/index.js";
 
 // __dirname をESMで再構築
 const __filename = fileURLToPath(import.meta.url);
